2fdd21c174ac3e902ff7491da751fc81ffc3044e
Mister-39 <71770484+Mister-39@users.noreply.github.com>
2020-12-13 16:57:02 +0200

    проект

---
+++ Ек.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+x=[0,99.99]
+y=[0,4.14]
+plt.plot(x,y,color='r',label='изменение кинетической энергии монеты')
+plt.xlabel('% от скорости света')
+plt.ylabel('Кинетичекая энергия монеты (МегаТонн)')
+plt.legend()
+plt.grid()
+plt.show()
+
