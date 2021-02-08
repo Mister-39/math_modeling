2bf3cd532bca953b9bb3c10b1f76fe4b7eab1097
Mister-39 <71770484+Mister-39@users.noreply.github.com>
2021-02-08 17:31:44 +0200

    Add files via upload

--- lab8_dop_ex_1.py
+++ lab8_dop_ex_1.py
@@ -1,15 +1,15 @@
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
-x=np.arange(0,h)
+x=np.arange(0,h-R,100)
 def gg_function(v,x):
     r=R+h-x
-    dvdx=G*M/r**2
+    dvdx=G*M/(v*r**2)
     return dvdx
-v_0=0
+v_0=0.0001
 G=6.67*10**(-11)
 M=5.97*10**(24)
-h=3000000
+h=30000000
 R=6300000
 solve_Bi = odeint(gg_function,v_0,x )
 plt.plot(x,solve_Bi[:,0], label='lenpop')
